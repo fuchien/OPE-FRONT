5b8d13abcb4668d9e77a1835c81b35a3f690b006
Chiien <fuchienhsu12@gmail.com>
2019-04-25 08:36:18 -0300

    Feat(Header) add header component and navbar

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  public navList: {}[] = [
+    {
+      text: `Página inicial`
+    },
+    {
+      text: `Sobre Nós`,
+      subList: [
+        {
+          text: `Quem somos nós?`
+        },
+        {
+          text: `Proposta Pedagógica`
+        }
+      ]
+    },
+    {
+      text: `Ciclos de Atuação`,
+      subList: [
+        {
+          text: `Educação Infantil`
+        },
+        {
+          text: `Anos Iniciais`
+        },
+        {
+          text: `Anos Finais`
+        },
+        {
+          text: `Ensino Médio`
+        }
+      ]
+    },
+    {
+      text: `Informativos`
+    },
+    {
+      text: `Contato`
+    }
+  ]
+
+  public navClicked: string = '';
+  public menuOpened: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  handleNavClick (link: string): void {
+    this.navClicked = link;
+  }
+
+  handleMenu (): void {
+    this.menuOpened = !this.menuOpened;
+  }
+
+}
